a323244a50aba6bb100255c4a9b12ff4dc68e8af
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-27 22:40:08 +0200

    Add solution to exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedF = (fahrenheit - 32) * (5/9);
+  return Math.round(convertedF * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedC = celsius * (9/5) + 32;
+  return Math.round(convertedC * 10) / 10;
 };
 
 // Do not edit below this line

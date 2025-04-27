a06c7e41403de45f5b66955a22c07e2ce20a3b86
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-27 20:23:10 +0200

    Add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(baseArray, ...toRemove){
+  let itemsToRemove = toRemove;
+
+  for (let i = 0; i < baseArray.length; i++){
+    for (let j = 0; j < itemsToRemove.length; j++){
+      baseArray = baseArray.filter(item => item !== itemsToRemove[j]); 
+    }
+  }
+  return baseArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

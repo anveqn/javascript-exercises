43b93896e841ca97b69418ad72daecbff3c82164
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-27 21:30:17 +0200

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  let sum = 0;
+  if(firstNumber < 0 || secondNumber < 0){
+    return "ERROR";
+  } else if(!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+    return "ERROR";
+  } else if (typeof firstNumber !== "number" || typeof secondNumber !== "number"){
+    return "ERROR";
+  } else {
+    if (firstNumber < secondNumber){
+      for (let i = firstNumber; i<=secondNumber; i++){
+        sum += i;
+      }
+    } else {
+      for (let i = secondNumber; i<=firstNumber; i++){
+        sum += i;
+      }
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line

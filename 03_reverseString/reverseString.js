59c5000cb4e9b0128b32d03107cb8f1943357313
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-27 18:05:12 +0200

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const dividedString = string.split("");
+  let finalString = "";
+  for (let i = string.length-1; i>=0; i--){
+     finalString += dividedString[i];
+  }
+  return finalString;
 };
 
 // Do not edit below this line
